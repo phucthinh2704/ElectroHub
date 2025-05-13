184794f6820d092cbb8957b4e3ba0c4d63645077
Tran Nguyen Phuc Thinh <thinhphuc2704@gmail.com>
2025-05-14 00:42:55 +0700

    Change some field in insert file

--- data/cate_brand.js
+++ data/cate_brand.js
@@ -2,14 +2,17 @@ const data = [
 	{
 		cate: "Smartphone",
 		brand: ["Apple", "Samsung", "LG", "Asus", "Nokia", "BlackBerry"],
+		image: "https://cdn.shopify.com/s/files/1/1903/4853/files/mobile-devices_300x.jpg?v=1613166682",
 	},
 	{
 		cate: "Tablet",
 		brand: ["iPad", "Samsung", "Acer", "Asus", "Lenovo", "Haier"],
+		image: "https://digital-world-2.myshopify.com/cdn/shop/files/pc-1_300x.jpg?v=1613166682",
 	},
 	{
 		cate: "Laptop",
 		brand: ["Asus", "Dell", "Acer", "Lenovo", "HP", "Macbook"],
+		image: "https://digital-world-2.myshopify.com/cdn/shop/files/laptop_300x.jpg?v=1613166811",
 	},
 	{
 		cate: "Accessories",
@@ -21,14 +24,34 @@ const data = [
 			"Mouse",
 			"Cases",
 		],
+		image: "https://digital-world-2.myshopify.com/cdn/shop/files/headphone_8467ba67-70c7-4977-b57e-8847d56549c6_300x.jpg?v=1613166811",
 	},
 	{
 		cate: "Television",
 		brand: ["Apple", "Samsung", "LG", "Asus", "Nokia", "BlackBerry"],
+		image: "https://digital-world-2.myshopify.com/cdn/shop/files/television_300x.jpg?v=1613166810",
 	},
 	{
 		cate: "Printer",
 		brand: ["Samsung", "Acer", "Asus", "Lenovo", "Haeir"],
+		image: "https://digital-world-2.myshopify.com/cdn/shop/files/printer_300x.jpg?v=1613166810",
+	},
+	{
+		cate: "Camera",
+		brand: ["Canon", "Nikon", "Sony", "Fujifilm", "GoPro"],
+		image: "https://digital-world-2.myshopify.com/cdn/shop/products/z1_1_400x.jpg?v=1491404913",
+	},
+	{
+		cate: "Speaker",
+		brand: [
+			"Apple",
+			"Samsung",
+			"LG",
+			"JBL",
+			"Bose",
+			"Sony",
+		],
+		image: "https://digital-world-2.myshopify.com/cdn/shop/products/z2_2_400x.jpg?v=1491404907",
 	},
 ];
 
